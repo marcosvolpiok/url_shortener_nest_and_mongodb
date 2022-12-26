6ae94a50e8ec223409bd38cbed0346bbaa7159de
Marcos Volpi <marcos@marcosvolpi.com>
2022-12-26 10:22:13 -0300

    Feat(App) Add Mongo and ConfigModule Imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`, {
+       useNewUrlParser: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
